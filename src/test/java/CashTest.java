d3f6608234605195c90c3111f05f6a97a0bcfcb9
ccx1010sky <ccx1010@gmail.com>
2022-02-14 15:36:29 +0000

    CashTest completed

--- src/test/java/CashTest.java
+++ src/test/java/CashTest.java
@@ -1,7 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CashTest {
     Cash cash;
+    @Before
+    public void before(){
+        cash = new Cash(100.00);
+    }
+
+    @Test
+
+    public void hasAmount(){
+        assertEquals(100.00,cash.getAmount(),0.00);
+    }
 
 
 }
